9f3e0095e4eb11f22b07d390b8bead11f2bcfe82
Jônatas Silverio <daniel_jhonny@hotmail.com>
2025-04-22 09:00:28 -0300

    update files

--- rest-geoserver.py
+++ rest-geoserver.py
@@ -1,14 +1,49 @@
 import requests
+from requests.auth import HTTPBasicAuth
+from dotenv import load_dotenv
+import os
+from geo.Geoserver import Geoserver
+from flask import Flask, render_template
 
-url = "https://sistemas.itti.org.br/geoserver/rest/"
-response = requests.get(url) # Use requests.post, put, delete for other methods
 
-try:
-    response = requests.get(url)
-    response.raise_for_status() # Raises an exception for bad status codes
-    data = response.json()
-    print(data)
-except requests.exceptions.RequestException as e:
-    print(f"Request error: {e}")
-except ValueError:
-    print("Failed to decode JSON")
+load_dotenv()
+username = os.getenv("user")
+password = os.getenv("password")
+
+
+geoserver_url = "https://sistemas.itti.org.br/geoserver/rest/layers"
+
+
+params = {
+    "outputFormat": "application/html"
+}
+
+response = requests.get(geoserver_url, params = params, auth=HTTPBasicAuth(username, password))
+
+if response.status_code == 200:
+    dados = response.json()
+    layers = dados['layers']['layer'];
+    # for layer in layers:
+    # 	print(layer["name"])
+else:
+    print("Erro:", response.status_code)
+
+print("Testando meus layers")
+
+print(layers[1])
+
+app = Flask(__name__)
+
+# Rota principal
+@app.route('/')
+def home():
+    return render_template("index.html",layers=layers)
+
+# Outra rota
+@app.route('/sobre')
+def sobre():
+    return "<h2>Sobre o projeto</h2><p>Feito com Flask!</p>"
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
